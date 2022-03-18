3e53d8f15152bc7f6961cd0d5b938629e41eb314
tobyjohansen <83167511+tobyjohansen@users.noreply.github.com>
2022-03-18 13:29:17 +0100

    Add files via upload

---
+++ Person_has_Risikogruppe.sql
@@ -0,0 +1,13 @@
+SELECT * FROM mydb.person_has_risikogrupppe;
+SELECT * FROM mydb.person;
+SELECT * FROM mydb.risikogrupppe;
+
+
+INSERT INTO mydb.person_has_risikogrupppe (person_personnummer, risikogrupppe_risikogruppeID)
+VALUES ('01020311228', '9');
+
+INSERT INTO mydb.person_has_risikogrupppe (person_personnummer, risikogrupppe_risikogruppeID)
+VALUES ('07048735328', '9');
+
+INSERT INTO mydb.person_has_risikogrupppe (person_personnummer, risikogrupppe_risikogruppeID)
+VALUES ('05039387238', '9');
---
+++ person.sql
@@ -0,0 +1,12 @@
+SELECT * FROM mydb.person;
+
+SELECT * FROM mydb.post;
+
+INSERT INTO mydb.person (personnummer, navn, telefonnummer, adresse, post_PostNr)
+VALUES ('01020311228', 'Tom Henrik', '22225555', 'Staveien 22', '1501');
+
+INSERT INTO mydb.person (personnummer, navn, telefonnummer, adresse, post_PostNr)
+VALUES ('07048735328', 'Robert Henrik', '36059234', 'Staveien 22', '1501');
+
+INSERT INTO mydb.person (personnummer, navn, telefonnummer, adresse, post_PostNr)
+VALUES ('05039387238', 'Geir Erikson', '34487609', 'Fredsveien 98', '1601');
